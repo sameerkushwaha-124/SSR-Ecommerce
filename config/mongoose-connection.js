339a5f3f813ee88b61159ebdc5dc3e8954359a49
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2025-04-14 13:14:19 +0530

    env and connection changes

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,15 +1,25 @@
-const mongoose = require('mongoose');
-const debug = require('debug')("development:mongoose");
-const config = require('config'); 
+const mongoose = require("mongoose");
+const debug = require("debug")("development:mongoose");
+const config = require("config");
 
-
-mongoose.connect(`${config.get("MONGODB_URI")}/SSR-EcommerceDB`)
-.then(()=>{
-    debug("connected to database");
-})
-.catch((err)=>{
-    debug("error in connecting to database : " + err);
-})
-
+if (NODE_ENV === "production") {
+    mongoose
+    .connect(process.env.MONGODB_URI)
+    .then(() => {
+      debug("connected to database");
+    })
+    .catch((err) => {
+      debug("error in connecting to database : " + err);
+    });
+} else {
+  mongoose
+    .connect(`${config.get("MONGODB_URI")}/SSR-EcommerceDB`)
+    .then(() => {
+      debug("connected to database");
+    })
+    .catch((err) => {
+      debug("error in connecting to database : " + err);
+    });
+}
 
 module.exports = mongoose.connection;
